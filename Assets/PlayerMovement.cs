f2cf8264a28aa5af64b69f1f252769d8baa3313a
LIsheng927 <lisheng2@ualberta.ca>
2026-01-29 23:14:11 -0700

    Add player movement logic

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private PlayerControls controls;
+    public CharacterController characterController;
+
+    [Header("Movement Settings")]
+    [SerializeField] public float moveSpeed = 5f;
+    public Vector3 movementDirection;
+    public Vector2 MoveInput;
+    private Vector2 aimInput;
+
+    private void Awake()
     {
-        
+        controls = new   PlayerControls();
+
+        controls.Character.Movement.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
+        controls.Character.Movement.canceled += ctx => MoveInput = Vector2.zero;
+
+        controls.Character.Aim.performed += ctx => aimInput = ctx.ReadValue<Vector2>();
+        controls.Character.Aim.canceled += ctx => aimInput = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(MoveInput.x, 0, MoveInput.y);
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * Time.deltaTime * moveSpeed);
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
     }
 }
